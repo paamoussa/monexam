463b2b838c9a7df270028a1c428703137cd172df
zertox-tech <pamodiallo@gmail.com>
2020-12-25 16:52:20 +0000

    Mon message

---
+++ projectmoussa/src/main/java/com/tdsi/projectmoussa/ProjectmoussaApplication.java
@@ -0,0 +1,13 @@
+package com.tdsi.projectmoussa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmoussaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmoussaApplication.class, args);
+	}
+
+}
---
+++ projectmoussa/src/test/java/com/tdsi/projectmoussa/ProjectmoussaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdsi.projectmoussa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmoussaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
